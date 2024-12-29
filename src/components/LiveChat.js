a5b654709eaa17026b8176e7bcba7e4e349f9a08
bhargavi <bhargavi8055@gmail.com>
2024-12-29 14:18:00 +0530

    live chat

--- src/components/LiveChat.js
+++ src/components/LiveChat.js
@@ -17,7 +17,7 @@ const chatMessages = useSelector(store=>store.chat.messages)
             name:generateRandomName(),
             message:generateRandomMessage(20)
         }))
-    },2000)
+    },1000)
     return ()=>{clearInterval(interval)}
  },[])
   return (
